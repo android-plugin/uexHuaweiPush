3540d1d732bcb891f211a694c737d3920678ddba
ylt <510212753@qq.com>
2017-02-15 11:40:21 +0800

    支持环信

---
+++ src/org/zywx/wbpalmstar/plugin/uexhuaweipush/MyReceiver.java
@@ -0,0 +1,18 @@
+package org.zywx.wbpalmstar.plugin.uexhuaweipush;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ylt on 2017/2/15.
+ */
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
